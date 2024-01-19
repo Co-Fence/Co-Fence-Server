a08f0d8e24bb5eb4f1727ec91fcf875c643217d9
woogym <kwj9294@gmail.com>
2024-01-19 20:25:19 +0900

    신고 관리 기능api에 누락된 swagger코드 추가

--- co-fence/src/main/java/com/gdsc/cofence/controller/report/ReportController.java
+++ co-fence/src/main/java/com/gdsc/cofence/controller/report/ReportController.java
@@ -3,6 +3,7 @@ package com.gdsc.cofence.controller.report;
 import com.gdsc.cofence.dto.reportNotice.reportRequest.ReportRegistrationRequestDto;
 import com.gdsc.cofence.dto.reportNotice.reportResponse.ReportRegistrationResponseDto;
 import com.gdsc.cofence.service.report.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping("/register")
+    @Operation(summary = "신고등록하는 api", description = "requestBody를 통해서 신고관련 정보를 받아서 해당 신고에 대한 정보를 반환합니다. ")
     public ResponseEntity<ReportRegistrationResponseDto> registerReport(@RequestBody ReportRegistrationRequestDto requestDto, Principal principal) {
         ReportRegistrationResponseDto responseDto = reportService.registerReport(requestDto, principal);
 
